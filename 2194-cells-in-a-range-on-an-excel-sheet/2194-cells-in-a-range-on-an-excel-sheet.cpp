dbfe73afbb8feec7170e0bd03cf5dc1501d919d2
Anshu Sonkar <anshusonkar045@gmail.com>
2026-09-02 00:13:53 +0530

    Time: 0 ms (100%), Space: 10.6 MB (77.08%) - LeetHub

---
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> cellsInRange(string s) {
+        vector<string> ans;
+
+        char startCol = s[0];
+        char startRow = s[1];
+
+        char endCol = s[3];
+        char endRow = s[4];
+
+        for (char col = startCol; col <= endCol; col++) {
+            for (char row = startRow; row <= endRow; row++) {
+                string cell = "";
+
+                cell += col;
+                cell += row;
+
+                ans.push_back(cell);
+            }
+        }
+
+        return ans;
+    }
+};
